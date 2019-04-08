1a854fc277cb62f76711b6a89119a1251e7257b9
Jenn Lao <jenn.p.lao@gmail.com>
2019-04-08 08:36:57 -0600

    Add functionaliy to zoom on a location using search bar

--- main.bundle.js
+++ main.bundle.js
@@ -52,8 +52,17 @@
 	mapboxgl.accessToken = 'pk.eyJ1IjoianBsYW8iLCJhIjoiY2p1NHE2emUzMGdicTQ0bzJwdW91aGZ1MCJ9.xOcYJbcXocOi1wogWwWv6w';
 	var map = new mapboxgl.Map({
 	  container: 'map',
-	  style: 'mapbox://styles/mapbox/streets-v11'
+	  style: 'mapbox://styles/mapbox/streets-v11',
+	  center: [-105.27, 40],
+	  zoom: 1.5
 	});
+
+	var geocoder = new MapboxGeocoder({
+	  accessToken: mapboxgl.accessToken
+	});
+
+	map.addControl(new mapboxgl.NavigationControl());
+	map.addControl(geocoder);
 
 /***/ }),
 /* 1 */
